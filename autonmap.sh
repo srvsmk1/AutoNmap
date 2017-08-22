f8d3685b89231f64178ad1bb2c95a7bbeff87abf
srvsmk1 <mservas@ivytech.edu>
2017-08-22 13:52:40 -0400

    Upload autonmap.sh
    Made changes to sendmail + HTML reports.

--- autonmap.sh
+++ autonmap.sh
@@ -5,17 +5,17 @@ DATE=`date +%F`
 ## Begin Config
 
 # The directory for autonmap data/scans
-RUN_DIRECTORY="/usr/local/autonmap/"
+RUN_DIRECTORY="/opt/autonmap/"
 
 # The directory you want the web report to live in
-WEB_DIRECTORY="/var/www/autonmap/"
+WEB_DIRECTORY="/var/www/html/"
 
-# The subnets you want to scan daily, space seperated. 
-SCAN_SUBNETS="10.101.0.0/24"
+# The subnets you want to scan daily, space seperated.
+SCAN_SUBNETS="192.168.1.0/24 192.168.2.0/24"
 
 # The full path (http) to where the report will be hosted by your webserver. This is included in the email report.
-# I suggest setting up auth using htpasswd etc, in which case you can include the auth in the URL for simplicity if you want. 
-WEB_URL="http://mywebserver.com/autonmap/scan-$DATE.xml"
+# I suggest setting up auth using htpasswd etc, in which case you can include the auth in the URL for simplicity if you want.
+WEB_URL="http://192.168.1.2/scan-$DATE.html"
 
 # The full path to your chosen nmap binary
 NMAP="/usr/bin/nmap"
@@ -23,12 +23,12 @@ NMAP="/usr/bin/nmap"
 # The path to the ndiff tool provided with nmap
 NDIFF="/usr/bin/ndiff"
 
-# The email address(es), space seperated that you wish to send the email report to. 
-EMAIL_RECIPIENTS="you@yourdomain.com youteam@yourdomain.com"
+# The email address(es), space seperated that you wish to send the email report to.
+EMAIL_RECIPIENTS="1@example.com 2@example.com"
 
 ## End config
 
-echo "`date` - Welcome to AutoNmap2. "
+echo "`date` - Welcome to AutoNmap2."
 
 # Ensure we can change to the run directory
 cd $RUN_DIRECTORY || exit 2
@@ -48,22 +48,30 @@ then
     if [ `echo "$DIFF" | wc -l` -gt 2 ]
     then
             echo "`date` - Differences Detected. Sending mail."
-            echo -e "AutoNmap2 found differences in a scan for '${SCAN_SUBNETS}' since yesterday. \n\n$DIFF\n\nFull report available at $WEB_URL" | mail -s "AutoNmap2" $EMAIL_RECIPIENTS
+#Added a for loop to send the emails because sending the email to 3 recipients was getting kicked back by Office365.
+            for RECIPIENTS in $EMAIL_RECIPIENTS
+            do
+            echo -e "AutoNmap2 found differences in a scan for '${SCAN_SUBNETS}' since yesterday. \n\n$DIFF\n\nFull report available at $WEB_URL" | mail -r "from@address.com" -s "Port Scan Changes $DATE" -S 'smtp=smtp://smtpserver.email:25' $RECIPIENTS
+            done
     else
             echo "`date`- No differences, skipping mail. "
     fi
 
-else 
+else
     echo "`date` - There is no previous scan (scan-prev.xml). Cannot diff today; will do so tomorrow."
 fi
 
 # Copy the scan report to the web directory so it can be viewed later.
 echo "`date` - Copying XML to web directory. "
 cp scan-$DATE.xml $WEB_DIRECTORY
+echo "`date` - Converting XML to HTML."
+sed -i '/<?xml-stylesheet href="nmap.xsl" type="text\/xsl"?>/c\<?xml-stylesheet href="/usr/share/nmap/nmap.xsl" type="text/xsl"?>\' /var/www/html/scan-$DATE.xml
+xsltproc /var/www/html/scan-$DATE.xml -o /var/www/html/scan-$DATE.html
+rm /var/www/html/scan-$DATE.xml
 
 # Create the link from today's report to scan-prev so it can be used tomorrow for diff.
 echo "`date` - Linking todays scan to scan-prev.xml"
 ln -sf scan-$DATE.xml scan-prev.xml
 
 echo "`date` - AutoNmap2 is complete."
-exit 0 
+exit 0
